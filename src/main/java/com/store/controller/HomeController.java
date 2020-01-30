bb47ae75da4bfe1fe35226ea7117ac5b9a836025
myuser <baysik734@gmail.com>
2020-01-30 22:22:58 +0900

    テストコードの削除

--- src/main/java/com/store/controller/HomeController.java
+++ src/main/java/com/store/controller/HomeController.java
@@ -13,18 +13,6 @@ public class HomeController {
     @GetMapping("/home")
     public String getHome(Model model) {
         model.addAttribute("contents", "home :: home_contents");
-
-        System.out.println("test1");
-        System.out.println("test2");
-        System.out.println("test3");
-        System.out.println("test4");
-        System.out.println("test5");
-        System.out.println("test6");
-        System.out.println("test7");
-        System.out.println("test8");
-        System.out.println("test8");
-        System.out.println("test10");
-
         return "base";
     }
 }

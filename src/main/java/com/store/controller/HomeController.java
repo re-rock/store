94987fd551e969ee2e46c77c981db39ef88adffe
yuhei kobayashi <t-yuhkobayashi@yahoo-corp.jp>
2020-02-05 13:17:29 +0900

    test3

--- src/main/java/com/store/controller/HomeController.java
+++ src/main/java/com/store/controller/HomeController.java
@@ -17,6 +17,7 @@ public class HomeController {
 
         System.out.println("commit-1");
         System.out.println("commit-2");
+        System.out.println("commit-3");
 
         return "base";
     }

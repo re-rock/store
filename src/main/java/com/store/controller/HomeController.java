fd958c71ab94fbe3bf4e256837213382793c626c
yuhei kobayashi <t-yuhkobayashi@yahoo-corp.jp>
2020-02-05 13:33:01 +0900

    test4

--- src/main/java/com/store/controller/HomeController.java
+++ src/main/java/com/store/controller/HomeController.java
@@ -17,6 +17,7 @@ public class HomeController {
         System.out.println("test1");
         System.out.println("test2");
         System.out.println("test3");
+        System.out.println("test4");
         return "base";
     }
 }
